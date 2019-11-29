e3fdadc08dd5db250e0e2222d7dd35917ad2e85d
Nick Wittland <NickW@dspace.de>
2019-11-29 07:21:28 +0100

    feat: styled action bar in header

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -7,9 +7,15 @@ export const Head = styled.header`
   border-bottom: 4px #ffc107 solid;
   box-shadow: 0px 4px 4px #ffc107;
 
+  padding: 8px;
+`
+
+export const ActionBar = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-end;
   align-items: center;
 
-  padding: 8px;
+  & > :not(:first-child) {
+    margin-left: 8px;
+  }
 `
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,43 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import Button from 'react-bootstrap/Button'
-import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import FormControl from 'react-bootstrap/FormControl'
 import InputGroup from 'react-bootstrap/InputGroup'
+import Row from 'react-bootstrap/Row'
 
-import { Head } from './Header.styles'
+import { ActionBar, Head } from './Header.styles'
 
 const Header: React.FC = () => {
   return (
     <Head>
-      Head
-      <span>
-        <Button variant="secondary">
-          <FontAwesomeIcon icon="plus" />
-        </Button>
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text id="inputGroupPrepend">
-              <FontAwesomeIcon icon="search" />
-            </InputGroup.Text>
-          </InputGroup.Prepend>
-          <Form.Control
-            type="text"
-            placeholder="Search"
-            aria-describedby="inputGroupPrepend"
-          />
-        </InputGroup>
-      </span>
+      <Container fluid>
+        <Row>
+          <Col xs={12} md={8}>
+            Head
+          </Col>
+          <Col xs={12} md={4}>
+            <ActionBar>
+              <Button variant="secondary">
+                <FontAwesomeIcon icon="plus" />
+              </Button>
+              <InputGroup>
+                <InputGroup.Prepend>
+                  <InputGroup.Text id="inputGroupPrepend">
+                    <FontAwesomeIcon icon="search" />
+                  </InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl
+                  type="text"
+                  placeholder="Search..."
+                  aria-describedby="inputGroupPrepend"
+                />
+              </InputGroup>
+            </ActionBar>
+          </Col>
+        </Row>
+      </Container>
     </Head>
   )
 }
