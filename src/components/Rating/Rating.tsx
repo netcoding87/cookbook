730b1f394095e68a02de171f1f68a659cd0df11b
Your Name <you@example.com>
2019-12-08 21:52:53 +0100

    feat: added optional onChange handler

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -5,13 +5,19 @@ import * as ReactRating from 'react-rating'
 interface RatingProps {
   rating: number
   readonly?: boolean
+  onChange?: (value: number) => void
 }
 
-const Rating: React.FC<RatingProps> = ({ rating, readonly = false }) => {
+const Rating: React.FC<RatingProps> = ({
+  rating,
+  readonly = false,
+  onChange,
+}) => {
   return (
     <ReactRating.default
       readonly={readonly}
       initialRating={rating}
+      onChange={(value: number) => onChange && onChange(value)}
       emptySymbol={<FontAwesomeIcon icon={['fas', 'star']} color="lightgray" />}
       fullSymbol={<FontAwesomeIcon icon={['fas', 'star']} color="orange" />}
     />
