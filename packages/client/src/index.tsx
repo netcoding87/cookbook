e2cf8149e8299690696e559e69e92cdef954394a
Your Name <you@example.com>
2020-04-19 21:35:25 +0200

    fix: typo in import of refactored font aweseome icons

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import App from './components/App'
-import loadIcons from './fontawesomeIcons'
+import loadIcons from './fontAwesomeIcons'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
 
