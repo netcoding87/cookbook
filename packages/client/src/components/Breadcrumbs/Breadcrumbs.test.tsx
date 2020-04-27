fb04e814f3f7affe019b7136095fea9d053ce2a4
Your Name <you@example.com>
2020-04-27 21:28:55 +0200

    refactor: breadcrumps tests

--- packages/client/src/components/Breadcrumbs/Breadcrumbs.test.tsx
+++ packages/client/src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -4,36 +4,38 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 import Breadcrumbs from './Breadcrumbs'
 
-it('should render simple breadcrumbs with fake URL', () => {
-  render(
-    <Router>
-      <Breadcrumbs
-        items={[
-          { url: window.location.href, title: 'Lorem' },
-          { url: window.location.href, title: 'ipsum' },
-          { url: window.location.href, title: 'dolor' },
-          { url: window.location.href, title: 'sit' },
-          { url: window.location.href, title: 'amet' },
-          { url: window.location.href, title: 'consetetur' },
-          { url: window.location.href, title: 'sadipscing' },
-        ]}
-      />
-    </Router>
-  )
-})
+describe('<Breadcrumps />', () => {
+  it('should render simple breadcrumbs with fake URL', async () => {
+    render(
+      <Router>
+        <Breadcrumbs
+          items={[
+            { url: window.location.href, title: 'Lorem' },
+            { url: window.location.href, title: 'ipsum' },
+            { url: window.location.href, title: 'dolor' },
+            { url: window.location.href, title: 'sit' },
+            { url: window.location.href, title: 'amet' },
+            { url: window.location.href, title: 'consetetur' },
+            { url: window.location.href, title: 'sadipscing' },
+          ]}
+        />
+      </Router>
+    )
+  })
 
-it('should render empty title', () => {
-  render(
-    <Router>
-      <Breadcrumbs items={[{ url: window.location.href, title: '' }]} />
-    </Router>
-  )
-})
+  it('should render empty title', async () => {
+    render(
+      <Router>
+        <Breadcrumbs items={[{ url: window.location.href, title: '' }]} />
+      </Router>
+    )
+  })
 
-it('should render with empty URL', () => {
-  render(
-    <Router>
-      <Breadcrumbs items={[{ url: '', title: '' }]} />
-    </Router>
-  )
+  it('should render with empty URL', async () => {
+    render(
+      <Router>
+        <Breadcrumbs items={[{ url: '', title: '' }]} />
+      </Router>
+    )
+  })
 })
